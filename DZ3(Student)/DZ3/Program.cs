0554be93d6e258a158795ccf0d8a91563d836e7e
NikolaiHoncharov <Goncharovnik@ukr.net>
2018-03-16 16:51:19 +0200

    Class Student. Entering student data. Entering estimates. The output of all data and the average estimates in the subjects.

---
+++ DZ3(Student)/DZ3/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DZ3
+{
+    class Program
+    {
+        static Student std;
+        static void Main()
+        {
+            std = new Student();
+            int Enter = 0;
+
+            while (true)
+            {
+                WriteLine("\t\t**Меню программы**\nВведите:\n1 - для ввода даных о студенте;\n2 - для вывода информации о студенте;\n3 - для вывода среднего арифметического;\n4 - для выхода.\n");
+                bool isint = Int32.TryParse(ReadLine(), out Enter);
+                if (isint && Enter > 0 || Enter <5)
+                { 
+                    if (Enter == 1)
+                    {
+                        //Ввод дынных о студентне и оценках
+                        InputInfoStud();
+                    }
+                    if (Enter == 2)
+                    {
+                        Clear();
+                        WriteLine("\nВывод данных о студенте:\n");
+                        std.ShowInfo();
+                    }
+                    if (Enter == 3)
+                    {
+                        Clear();
+                        std.Sred();
+                    }
+                    WriteLine("\n");
+                    if (Enter == 4)
+                    {
+                        break;
+                    }
+                }
+                else { Clear(); WriteLine("Ошибка! Неправильный ввод\n"); }
+            }
+            ReadKey();
+        }
+
+        static void InputInfoStud() {
+            int temp = 0;
+            Write(" Введите имя студента: ");
+            std.FName = ReadLine();
+            Write(" Введите отчество студента: ");
+            std.SName = ReadLine();
+            Write(" Введите фамилию студента: ");
+            std.LName = ReadLine();
+            Write(" Введите возраст студента: ");
+            while (temp == 0) temp = Checking(ReadLine());
+            std.Age = temp;
+            Write(" Введите номер группу: ");
+            std.Group = ReadLine();
+            temp = 0;
+            Write(" Введите кол-во оценок по программированию: ");
+            while (temp == 0) temp = Checking(ReadLine());
+            std.AppraisalsProgr(temp);
+            temp = 0;
+            Write(" Введите кол-во оценок по администрированию: ");
+            while (temp == 0) temp = Checking(ReadLine());
+            std.AppraisalsAdminis(temp);
+            temp = 0;
+            Write(" Введите кол-во оценок по дизайну: ");
+            while (temp == 0) temp = Checking(ReadLine());
+            std.AppraisalsWeb(temp);
+
+        }
+        static int Checking(string str) {
+            int count = 0;
+            if (Int32.TryParse(str, out count)==false)
+            {
+                Write(" Ошибка ввода! Повторите\n");
+                return 0;
+            }
+            else return count;
+        }
+
+    }
+}
---
+++ DZ3(Student)/DZ3/Student.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DZ3
+{
+    public class Student
+    {
+        public string FName { get; set; }
+        public string SName { get; set; }
+        public string LName { get; set; }
+        public int Age { get; set; }
+        public string Group { get; set; }
+        int[] Programming;
+        int[] Administration;
+        int[] Web;
+
+       public void AppraisalsProgr(int size) {
+            Programming = new int[size];
+            WriteLine("Введите оценки:\n");
+            for (int i = 0; i < size; )
+            {
+                if (Int32.TryParse(ReadLine(), out Programming[i])) i++;
+                else WriteLine(" Ошибка ввода! Повторите ввод данных\n");
+            }
+        }
+        public void AppraisalsWeb(int size)
+        {
+            Web = new int[size];
+            WriteLine("Введите оценки:\n");
+            for (int i = 0; i < size;)
+            {
+                if (Int32.TryParse(ReadLine(), out Web[i])) i++;
+                else WriteLine(" Ошибка ввода! Повторите ввод данных\n");
+            }
+        }
+
+        public void AppraisalsAdminis(int size)
+        {
+            Administration = new int[size];
+            WriteLine("Введите оценки:\n");
+            for (int i = 0; i < size;)
+            {
+                if (Int32.TryParse(ReadLine(), out Administration[i])) i++;
+                else WriteLine(" Ошибка ввода! Повторите ввод данных\n");
+            }
+        }
+
+        public void ShowInfo() {
+            WriteLine($" Имя: {FName}\n Отчество: {SName}\n Фамилия: {SName}\n Возраст: {Age}\n Группа: {Group}\n");
+            if (Programming != null) {
+                Write("Оценки по программированию: ");
+                for (int i = 0; i < Programming.Length; i++)Write($"{Programming[i]} ");
+            }
+            if (Web != null)
+            {
+                Write("\nОценки по дизайну: ");
+                for (int i = 0; i < Web.Length; i++) Write($"{Web[i]} ");
+            }
+            if (Administration != null)
+            {
+                Write("\nОценки по администрированию: ");
+                for (int i = 0; i < Administration.Length; i++) Write($"{Administration[i]} ");
+            }
+
+        }
+        public void Sred() {
+            int sumPr = 0, sumWeb = 0, sumAdmin = 0;
+            if (Web != null && Programming != null && Administration != null)
+            {
+                for (int i = 0; i < Programming.Length; i++) sumPr += Programming[i];
+                WriteLine($"среднее арифметическое оценок по программированию :" + sumPr / Programming.Length);
+                for (int i = 0; i < Web.Length; i++) sumWeb += Web[i];
+                WriteLine($"\nсреднее арифметическое оценок по дизайну :" + sumWeb / Web.Length);
+                for (int i = 0; i < Administration.Length; i++) sumAdmin += Administration[i];
+                WriteLine($"\nсреднее арифметическое оценок по адинистрированию :" + sumAdmin / Administration.Length);
+            }
+            else WriteLine("Ошибка");
+        }
+    }
+}
