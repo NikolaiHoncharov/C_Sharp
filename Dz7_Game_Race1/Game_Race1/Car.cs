ef074766663905c3af4b42047a739f6820b9d792
NikolaiHoncharov <Goncharovnik@ukr.net>
2018-03-27 13:33:31 +0300

    Dz №7 Game'Race' Event

---
+++ Dz7_Game_Race1/Game_Race1/Car.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using static System.Console;
+
+namespace Game_Race1
+{
+    public delegate int Start();
+
+    abstract class Car
+    {
+        protected int speed;
+        protected string name;
+        protected int round;
+
+        public Car()
+        {
+            this.name = "John";
+            round = 0;
+        }
+        public abstract void finish(object sender, ExamEventArgs e);
+        public abstract int go();
+    }
+    class SportsCar : Car
+    {
+        Random random1;
+        //     public event EventHandler Finish;
+        public SportsCar(string name)
+        {
+            this.name = name;
+            round = 0;
+        }
+        public SportsCar() : base() { }
+        public override int go()
+        {
+            int round = 0;
+            random1 = new Random();
+            speed += random1.Next(3, 12);
+            while (this.speed <= 100)
+            {
+
+                speed += random1.Next(3, 18);
+                if (speed < 100)
+                    Write($"Игрок  {name}  преодолел {speed}% всей гонки за {round + 1} круг\n");
+                else Write($"Игрок  {name}  преодолел 100% всей гонки за {round + 1} круг\n");
+                round++;
+            }
+            return round;
+            //throw new NotImplementedException();
+        }
+        public override void finish(object sender, ExamEventArgs e)
+        {
+            WriteLine($"\nПОБЕДА!\n {name} прошел 100% с кол-вом кургов {e.Task} ");
+        }
+    }
+
+    class PassengerCar : Car
+    {
+        //  public event EventHandler Finish;
+        Random random;
+        public PassengerCar(string name)
+        {
+            this.name = name;
+            round = 0;
+        }
+        public PassengerCar() : base() { }
+        public override int go()
+        {
+            int round = 0;
+            random = new Random();
+            speed += random.Next(1, 11);
+            while (this.speed <= 100)
+            {
+
+                speed += random.Next(1, 17);
+                if (speed < 100)
+                    Write($"Игрок {name} преодолел {speed}% всей гонки за {round + 1} круг\n");
+                else Write($"Игрок  {name}  преодолел 100% всей гонки за {round + 1} круг\n");
+                round++;
+            }
+            return round;
+            //throw new NotImplementedException();
+        }
+        public override void finish(object sender, ExamEventArgs e)
+        {
+            WriteLine($"\nПОБЕДА!\n {name} прошел 100% с кол-вом кургов {e.Task}");
+        }
+    }
+    class ExamEventArgs : EventArgs
+    {
+        public string Task { get; set; }
+    }
+    class Game
+    {
+        public EventHandler<ExamEventArgs> Finish;
+        public void finish(ExamEventArgs task)
+        {
+            if (Finish != null)
+                Finish(this, task);
+        }
+    }
+}
---
+++ Dz7_Game_Race1/Game_Race1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using static System.Console;
+
+namespace Game_Race1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game win = new Game();
+            string name1, name2;
+            int kol = 0, kol2 = 0;
+            WriteLine("Введите имя 1 гонщика: ");
+            name1 = ReadLine();
+            SportsCar one = new SportsCar(name1);
+            WriteLine("Введите имя 2 гонщика: ");
+            name2 = ReadLine();
+            PassengerCar two = new PassengerCar(name2);
+
+            int mode, enter1;
+            bool flag = true, temp = false;
+            WriteLine("\nВведите 1 - для начала\nВведите 0 - для выхода программы");
+
+
+            do
+            {
+                temp = Int32.TryParse(ReadLine(), out mode);
+                if (temp == false || (mode < 0 || mode > 1)) WriteLine("Ошибка вводв данных! Повторите ввод.");
+            } while (temp == false || (mode < 0 || mode > 1));
+
+            if (mode == 1) flag = true;
+            else if (mode == 0) flag = false;
+            while (flag != false)
+            {
+                while (mode != 0)
+                {
+                    Write("\n Возможности данной прогаммы:\n" + " Введите: \n 1 - для начала гонки; \n 2 - для вывода победителя;");
+                    Write("\nСделайте выбор: "); enter1 = Convert.ToInt32(ReadLine());
+                    switch (enter1)
+                    {
+                        case 1:
+                            {
+                                Clear();
+                                Start d1 = one.go;
+                                kol = d1();
+                                Start d2 = two.go;
+                                kol2 = d2();
+                            }
+                            break;
+                        case 2:
+                            {
+                                Clear();
+                                if (kol > kol2)
+                                {
+                                    win.Finish += two.finish;
+                                    ExamEventArgs eventArgs = new ExamEventArgs { Task = Convert.ToString(kol2) };
+                                    win.finish(eventArgs);
+                                }
+                                else
+                                {
+                                    win.Finish += one.finish;
+                                    ExamEventArgs eventArgs = new ExamEventArgs { Task = Convert.ToString(kol) };
+                                    win.finish(eventArgs);
+                                }
+                            }
+                            break;
+                        default: Write("\nВы сделали некорректный выбор\n"); break;
+                    }
+                    Write("\nВведите 1 - для продолжения\nВведите 0 - чтобы выйти\n");
+
+                    do
+                    {
+                        temp = Int32.TryParse(ReadLine(), out mode);
+                        if (temp == false || (mode < 0 || mode > 1)) WriteLine("Ошибка вводв данных! Повторите ввод.");
+                    } while (temp == false || (mode < 0 || mode > 1));
+
+                    if (mode == 1) flag = true;
+                    else if (mode == 0) flag = false;
+
+                }
+            }
+        }
+    }
+}
